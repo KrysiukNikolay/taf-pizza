dce2d4cd13189e825537d1d0874b45ffeb4ea1c6
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-21 12:24:36 +0300

    refactor project

--- src/test/java/krysiuknikolay/TerrapizzaPage.java
+++ src/test/java/krysiuknikolay/TerrapizzaPage.java
@@ -1,4 +1,15 @@
 package krysiuknikolay;
 
 public class TerrapizzaPage {
+    public static final String LINK_BASE_PAGE = "https://terrapizza.by/";
+
+    public static final String BUTTON_MENU = "//a[@href='https://terrapizza.by/menu']";
+    public static final String BUTTON_PIZZA = "/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[9]/a";
+    public static final String BUTTON_CART = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
+    public static final String BUTTON_CLOSE_COOKIE = "//button[@class='close-icon']";
+    public static final String BUTTON_ADD_MARGARITA_IN_CART = "//div[@class='cart-button']//button[@data-id='356']";
+
+    public static final String LABEL_MARGARITA_IN_CART  = "//div[@class='basket__products-item-name']";
+
+
 }
--- src/test/java/krysiuknikolay/TerrapizzaTest.java
+++ src/test/java/krysiuknikolay/TerrapizzaTest.java
@@ -1,28 +1,43 @@
 package krysiuknikolay;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Assertions;
 
 import java.time.Duration;
 
 public class TerrapizzaTest {
     WebDriver driver;
 
-    @Before
+    @BeforeEach
     public void warmUP() {
         driver = new ChromeDriver();
+        driver.get(TerrapizzaPage.LINK_BASE_PAGE);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
     @Test
+        public void testTerrapizza() {
+        driver.findElement(By.xpath(TerrapizzaPage.BUTTON_CLOSE_COOKIE)).click();
+        driver.findElement(By.xpath(TerrapizzaPage.BUTTON_MENU)).click();
+        driver.findElement(By.xpath(TerrapizzaPage.BUTTON_PIZZA)).click();
+        driver.findElement(By.xpath(TerrapizzaPage.BUTTON_ADD_MARGARITA_IN_CART)).click();
+        driver.findElement(By.xpath(TerrapizzaPage.BUTTON_CART)).click();
+        String actual = driver.findElement(By.xpath(TerrapizzaPage.LABEL_MARGARITA_IN_CART)).getText();
+        Assertions.assertEquals("Пицца Венеция Классическая 32 см", actual);
 
-    @After
+
+    }
+
+    @AfterEach
     public void out() {
         driver.quit();
+
     }
 }
